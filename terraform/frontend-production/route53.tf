4f39e1fa090cf130b3659b7bb9046c529f71b779
Rindy Portfolio <rindy@tadpole.cc>
2026-01-01 13:04:03 -0500

    Add proper production deployment plan

---
+++ terraform/frontend-production/route53.tf
@@ -0,0 +1,12 @@
+# A record pointing to CloudFront distribution
+resource "aws_route53_record" "frontend" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.frontend.domain_name
+    zone_id                = aws_cloudfront_distribution.frontend.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
